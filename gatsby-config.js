724e9525bc45814c7c65610d4985ca528c4961ca
dharanchit <anchit@1stmain.co>
2021-09-25 00:06:38 +0530

    added g tag

--- gatsby-config.js
+++ gatsby-config.js
@@ -33,6 +33,22 @@ module.exports = {
       },
     },
     {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          "G-HJM3VWW433", // Google Analytics / GA
+        ],
+        // This object gets passed directly to the gtag config command
+        // This config will be shared across all trackingIds
+        // This object is used for configuration specific to this plugin
+        pluginConfig: {
+          // Puts tracking script in the head instead of the body
+          head: true,
+        },
+      },
+    },
+    {
       resolve: "gatsby-plugin-google-tagmanager",
       options: {
         id: "GTM-MFBWHF7",
