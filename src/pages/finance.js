f370c7e7c3b6a88c6a0691f9917d29cdc3c8f5c9
dharanchit <anchit@1stmain.co>
2021-09-26 16:06:07 +0530

    added links on homepage

--- src/pages/finance.js
+++ src/pages/finance.js
@@ -2,10 +2,15 @@ import React from "react"
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 import styled from "styled-components"
+import jsonData from "../material/finance/data.json"
 
 const FinanceWrapper = styled.div`
   .heading {
     text-align: center;
+  }
+
+  .margin-top {
+    margin-top: 20px;
   }
 `
 
@@ -15,6 +20,18 @@ const Finance = () => {
       <Layout>
         <Seo title="Finance" />
         <h3 className="heading">Everything about finance and stocks</h3>
+        <ul>
+          {jsonData.map(data => (
+            <li>
+              <div className="margin-top">
+                <h3>{data.title}</h3>
+                <ul>
+                  <li>{data.description}</li>
+                </ul>
+              </div>
+            </li>
+          ))}
+        </ul>
       </Layout>
     </FinanceWrapper>
   )
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,11 @@
 import * as React from "react"
-
+import { Grid } from "@mui/material"
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 import file from "../files/resume.pdf"
 import { AiOutlineCloudDownload } from "react-icons/ai"
 import styled from "styled-components"
+import { AiFillGithub, AiFillLinkedin } from "react-icons/ai"
 
 const StyledWrapper = styled.div`
   .icon-class {
@@ -27,9 +28,34 @@ const IndexPage = () => {
     <StyledWrapper>
       <Layout>
         <Seo title="Home" />
-        <a download href={file} className="resume-text">
-          <span className="text-class">Resume</span> <AiOutlineCloudDownload />
-        </a>
+        <Grid container justifyContent="center">
+          <Grid item xs={12}>
+            <a download href={file} className="resume-text">
+              <span className="text-class">Resume</span>{" "}
+              <AiOutlineCloudDownload />
+            </a>
+          </Grid>
+          <Grid item xs={12} justifyContent="center">
+            <a
+              href="https://www.linkedin.com/in/anchitdhar/"
+              className="resume-text"
+              target="_blank"
+            >
+              <span className="text-class">Linked In</span>
+              <AiFillLinkedin />
+            </a>
+          </Grid>
+          <Grid item xs={12} justifyContent="center">
+            <a
+              href="https://github.com/dharanchit"
+              className="resume-text"
+              target="_blank"
+            >
+              <span className="text-class">Github</span>
+              <AiFillGithub />
+            </a>
+          </Grid>
+        </Grid>
       </Layout>
     </StyledWrapper>
   )
