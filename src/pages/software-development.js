4b0cddf6875662d7ec1f2656462ef9d9012113f8
dharanchit <anchit@1stmain.co>
2021-11-11 16:28:43 +0530

    kicked off tech blog

--- src/pages/software-development.js
+++ src/pages/software-development.js
@@ -2,10 +2,16 @@ import React from "react"
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 import styled from "styled-components"
+import jsonData from "../material/development/data.json"
+
 
 const SoftwareWrapper = styled.div`
   .heading {
     text-align: center;
+  }
+
+  .margin-top {
+    margin-top: 20px;
   }
 `
 
@@ -17,6 +23,18 @@ const SoftwareDevelopment = () => {
         <h3 className="heading">
           Sharing Latest Updates On My Journey To A Better Software Developer
         </h3>
+        <ul>
+          {jsonData.map(data => (
+            <li>
+              <div className="margin-top">
+                <h3>{data.title}</h3>
+                <ul>
+                  <li>{data.description}</li>
+                </ul>
+              </div>
+            </li>
+          ))}
+        </ul>
       </Layout>
     </SoftwareWrapper>
   )
